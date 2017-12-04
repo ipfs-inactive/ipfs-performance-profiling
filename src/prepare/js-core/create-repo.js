ba9854b6c214c04fe3a75ae6b215a9dbf1b424d8
Richard Schneider <makaretu@gmail.com>
2017-12-05 11:47:34 +1300

    test: use os.tmpdir()

--- src/prepare/js-core/create-repo.js
+++ src/prepare/js-core/create-repo.js
@@ -3,9 +3,11 @@
 const IPFSRepo = require('ipfs-repo')
 const Store = require('fs-pull-blob-store')
 const clean = require('./destroy-repo')
+const os = require('os')
+const path = require('path')
 
 module.exports = function createTempRepo (repoPath) {
-  repoPath = repoPath || '/tmp/ipfs-test-' + Math.random().toString().substring(2, 8)
+  repoPath = repoPath || path.join(os.tmpdir(), 'ipfs-test-' + Math.random().toString().substring(2, 8))
 
   const repo = new IPFSRepo(repoPath, {
     bits: 1024,
