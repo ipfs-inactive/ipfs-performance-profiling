420914efd8fd7745459650b20004c1049d825c1a
Richard Schneider <makaretu@gmail.com>
2017-12-05 21:07:47 +1300

    fix: use hat() for unique names

--- src/prepare/js-core/create-repo.js
+++ src/prepare/js-core/create-repo.js
@@ -5,9 +5,10 @@ const Store = require('fs-pull-blob-store')
 const clean = require('./destroy-repo')
 const os = require('os')
 const path = require('path')
+const hat = require('hat')
 
 module.exports = function createTempRepo (repoPath) {
-  repoPath = repoPath || path.join(os.tmpdir(), 'ipfs-test-' + Math.random().toString().substring(2, 8))
+  repoPath = repoPath || path.join(os.tmpdir(), 'ipfs-test-' + hat())
 
   const repo = new IPFSRepo(repoPath, {
     bits: 1024,
