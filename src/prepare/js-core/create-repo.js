d8542a8eb475969e45b2ea90c55b4b544d6a0f10
Richard Schneider <makaretu@gmail.com>
2017-12-05 21:10:54 +1300

    test: use os.tmpdir() (#12)
    * test: use os.tmpdir()
    
    * fix: use hat() for unique names

--- src/prepare/js-core/create-repo.js
+++ src/prepare/js-core/create-repo.js
@@ -3,9 +3,12 @@
 const IPFSRepo = require('ipfs-repo')
 const Store = require('fs-pull-blob-store')
 const clean = require('./destroy-repo')
+const os = require('os')
+const path = require('path')
+const hat = require('hat')
 
 module.exports = function createTempRepo (repoPath) {
-  repoPath = repoPath || '/tmp/ipfs-test-' + Math.random().toString().substring(2, 8)
+  repoPath = repoPath || path.join(os.tmpdir(), 'ipfs-test-' + hat())
 
   const repo = new IPFSRepo(repoPath, {
     bits: 1024,
